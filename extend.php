b48cacdb3f456cd3d296a7e5d3bdcf79f0001c53
Cengizhan00 <108155912+Cengizhan00@users.noreply.github.com>
2023-07-17 23:21:58 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,24 +1,32 @@
 <?php
 
-/*
- * This file is part of akademi/flarum-ext-akademiform.
- *
- * Copyright (c) 2023 Yusuf.
- *
- * For the full copyright and license information, please view the LICENSE.md
- * file that was distributed with this source code.
- */
-
-namespace Akademi\\Akademiform;
-
+use Flarum\Discussion\Event\Saving;
 use Flarum\Extend;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less'),
-    (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/less/admin.less'),
-    new Extend\Locales(__DIR__.'/locale'),
+        ->css(__DIR__.'/resources/less/forum.less')
+        ->js(__DIR__.'/js/dist/forum.js'),
+    function (Dispatcher $events, SettingsRepositoryInterface $settings) {
+        $events->listen(Saving::class, function (Saving $event) use ($settings) {
+            $actor = $event->actor;
+            $discussion = $event->discussion;
+
+            if ($discussion->exists && !$actor->can('startDiscussion', $discussion->category)) {
+                // User doesn't have permission to start a discussion in this category
+                return;
+            }
+
+            if ($discussion->exists && $discussion->user_id !== $actor->id) {
+                // User is not the author of this discussion
+                return;
+            }
+
+            if (!$discussion->exists && $settings->get('flarum.my_discussions.only_own_discussions')) {
+                $event->discussion->user_id = $actor->id;
+            }
+        });
+    }
 ];
